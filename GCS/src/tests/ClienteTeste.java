c45f3ab688474b918108998e5ffac7cfa7399ffa
Nolascao <69157914+Nolascao@users.noreply.github.com>
2023-12-04 19:20:35 -0300

    feature
    Novos testes cliente

--- GCS/src/tests/ClienteTeste.java
+++ GCS/src/tests/ClienteTeste.java
@@ -7,21 +7,42 @@ import java.util.LinkedList;
 import java.util.List;
 
 import org.junit.Test;
+import org.junit.jupiter.api.BeforeEach;
+
 import entities.*;
 import enums.*;
 
 public class ClienteTeste {
 
+    public Cliente c;
+    public Jogo j;
+    public Jogo j1;
+    public Jogo j2;
+    public Compra co;
 
     @Test
     public void testarIncluirCompras(){
-
-        Cliente c = new Cliente("Matheus", "senha", "email");
-        Jogo j = new Jogo("Jogo",100,JogoCategoria.REGULAR);
-        Compra co = new Compra(c, LocalDate.now(), j);
+        c = new Cliente("Matheus", "senha", "email");
+        j = new Jogo("Jogo",100,JogoCategoria.REGULAR);
+        co = new Compra(c, LocalDate.now(), j);
         c.incluirCompra(co);
         List<Compra> compras = new LinkedList<>();
         compras = c.getCompras();
         assertEquals(co, compras.get(0));
     }
+
+     @Test
+    public void testarValorPagoCompras(){
+        c = new Cliente("Matheus", "senha", "email");
+        j = new Jogo("Jogo",100,JogoCategoria.REGULAR);
+        j1 = new Jogo("Jogo",250,JogoCategoria.LANCAMENTO);
+        co = new Compra(c, LocalDate.now(), j);
+        co.incluirJogo(j1);
+        c.incluirCompra(co);
+        List<Compra> compras = new LinkedList<>();
+        compras = c.getCompras();
+        assertEquals(co.valorAPagar(), c.totalEmCompras());
+    }
+
+
 }
