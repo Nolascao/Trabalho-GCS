15b9ab67dfa99d050c37db6ff3ad85c9b581dc53
Nolascao <69157914+Nolascao@users.noreply.github.com>
2023-12-04 18:24:12 -0300

    Feature - Teste Cliente
    Testando a função incluirCompras() da classe cliente.

--- GCS/src/tests/ClienteTeste.java
+++ GCS/src/tests/ClienteTeste.java
@@ -1,5 +1,27 @@
 package tests;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import java.time.LocalDate;
+import java.util.LinkedList;
+import java.util.List;
+
+import org.junit.Test;
+import entities.*;
+import enums.*;
+
 public class ClienteTeste {
 
+
+    @Test
+    public void testarIncluirCompras(){
+
+        Cliente c = new Cliente("Matheus", "senha", "email");
+        Jogo j = new Jogo("Jogo",100,JogoCategoria.REGULAR);
+        Compra co = new Compra(c, LocalDate.now(), j);
+        c.incluirCompra(co);
+        List<Compra> compras = new LinkedList<>();
+        compras = c.getCompras();
+        assertEquals(co, compras.get(0));
+    }
 }
